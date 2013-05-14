vliaskov/qemu-kvm

Implement "info memory-total" and "query-memory-total"
Returns total physical memory available to guest in bytes, including hotplugged
memory. Note that the number reported here may be different from what the guest
sees e.g. if the guest has not logically onlined hotplugged memory.

This functionality is provided independently of a balloon device, since a
guest can be using ACPI memory hotplug without using a balloon device.

v3->v4: Moved qmp command implementation to vl.c. This prevents a circular
header dependency problem.

Signed-off-by: Vasilis Liaskovitis <vasilis.liaskovitis@profitbricks.com>

monitor.c
@@ -2708,6 +2708,13 @@ static mon_cmd_t info_cmds[] = {
         .mhandler.info = hmp_info_balloon,
     },
     {
+        .name       = "memory-total",
+        .args_type  = "",
+        .params     = "",
+        .help       = "show total memory size",
+        .mhandler.info = hmp_info_memory_total,
+    },
+    {
         .name       = "qtree",
         .args_type  = "",
         .params     = "",

taohu/qemu

monitor.c
@@ -2729,6 +2729,13 @@ static mon_cmd_t info_cmds[] = {
         .mhandler.cmd = hmp_info_balloon,
     },
     {
+        .name       = "memory-total",
+        .args_type  = "",
+        .params     = "",
+        .help       = "show total memory size",
+        .mhandler.cmd = hmp_info_memory_total,
+    },
+    {
         .name       = "qtree",
         .args_type  = "",
         .params     = "",